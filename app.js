8f95e64e79505bc927c063a7e780be7f216e6d92
xkeekyx <xkeekyx@hotmail.com>
2019-01-13 16:11:31 -0800

    updated portfolio as of Jan 13

---
+++ app.js
@@ -0,0 +1,34 @@
+// // $(document).ready(function(){
+// //     //1. link to firebase
+// //     var subscribe = new Firebase("https://keyanna-s-portfolio.firebaseio.com/")
+// // })
+
+
+// $(document).ready(function(){
+//     if ($("#newContact").length>0){
+//         contactScript("forcontact");
+//     }
+// });
+
+//      // Initialize Firebase
+//      var config = {
+//         apiKey: "AIzaSyBjO-MgX8nUlStNXJ4WK98quHnd8RKqPpk",
+//         authDomain: "keyanna-s-portfolio.firebaseapp.com",
+//         databaseURL: "https://keyanna-s-portfolio.firebaseio.com",
+//         projectId: "keyanna-s-portfolio",
+//         storageBucket: "keyanna-s-portfolio.appspot.com",
+//         messagingSenderId: "1044795276395"
+//     };
+//     firebase.initializeApp(config);
+//     var b = firebase.database().ref("messages");
+//     $("#newContact").submit(function(config) {$(this), console.log ("Submit to Firebase");
+// var c=$("#name").val(),
+//     d=$("#email").val(),
+//     f={name:c, email:d};
+// return b.push(f).then(function(config) {
+//     $(".sucess").css("display","block"),
+//     $(".sucess-none").css("display","none")
+// }), !1 })
+
+
+
